e078deb3b82a78e5684869765f424d357ec4887d
jcmartinssh <arq.joaquim@gmail.com>
2023-08-09 01:30:13 -0300

    desenvolvimento script kits

--- src/AR_CodMun.R
+++ src/AR_CodMun.R
@@ -1,8 +1,8 @@
-library(fuzzyjoin)
+# library(fuzzyjoin)
 library(dplyr)
 library(stringr)
 library(sf)
-library(arrow)
+# library(arrow)
 
 rm_accent <- function(str, pattern = "all") {
   # Rotinas e funções úteis V 1.0
@@ -56,86 +56,98 @@ rm_accent <- function(str, pattern = "all") {
   return(str)
 }
 
-AR_gpkg <- "../../areas_de_risco/areas_risco.gpkg"
+AR_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/AreasDeRisco.gpkg"
+lotes <- st_layers(AR_gpkg)
+lote_layer <- select.list(lotes[[1]], title = "lote:", graphics = TRUE)
+AR_lote <- st_read(AR_gpkg, layer = lote_layer) |> st_zm(drop = TRUE, what = "ZM")
+Ar_lote_col <- colnames(AR_lote)
+nome_lote_col <- select.list(Ar_lote_col, title = "nomes dos municípios:", graphics = TRUE)
+uf_lote_col <- select.list(Ar_lote_col, title = "sigla das UFs:", graphics = TRUE)
 
-municipios_2021 <-
-  read_parquet("../../../src/municipios_2021.parquet") %>%
-  mutate(chave2 = str_c(SIGLA, trimws(str_to_lower(rm_accent(NM_MUN))), sep = " ")) %>% 
-  mutate(chaveB = trimws(str_to_lower(rm_accent(NM_MUN))))
+AR_lote <- AR_lote |>
+  mutate(chave1 = paste(get(uf_lote_col), trimws(str_to_lower(rm_accent(get(nome_lote_col)))), sep = " "),
+         chaveA = trimws(str_to_lower(rm_accent(get(nome_lote_col)))))
 
-UF <- unique(municipios_2021$SIGLA)
+municipios_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/MUNICIPIOS.gpkg"
+municipios_ver <- st_layers(municipios_gpkg)
+municipios_layer <- select.list(municipios_ver[[1]], title = "base de municipios:", graphics = TRUE)
+municipios <- st_read(municipios_gpkg, layer = municipios_layer)
+mun_col <- colnames(municipios)
+nome_mun_col <- select.list(mun_col, title = "nomes dos municípios:", graphics = TRUE)
+cod_mun_col <- select.list(mun_col, title = "códigos dos municípios:", graphics = TRUE)
 
-col_municipios <- colnames(municipios_2021)
+municipios <- municipios %>%
+  mutate(cod_uf = substr(get(cod_mun_col), 1, 2)) |>
+  mutate(sigla_uf = case_when(cod_uf == "11" ~ "RO", cod_uf == "12" ~ "AC", cod_uf == "13" ~ "AM", cod_uf == "14" ~ "RR", cod_uf == "15" ~ "PA",
+                              cod_uf == "16" ~ "AP", cod_uf == "17" ~ "TO",
+                              cod_uf == "21" ~ "MA", cod_uf == "22" ~ "PI", cod_uf == "23" ~ "CE", cod_uf == "24" ~ "RN", cod_uf == "25" ~ "PB",
+                              cod_uf == "26" ~ "PE", cod_uf == "27" ~ "AL", cod_uf == "28" ~ "SE", cod_uf == "29" ~ "BA",
+                              cod_uf == "31" ~ "MG", cod_uf == "32" ~ "ES", cod_uf == "33" ~ "RJ", cod_uf == "35" ~ "SP",
+                              cod_uf == "41" ~ "PR", cod_uf == "42" ~ "SC", cod_uf == "43" ~ "RS",
+                              cod_uf == "50" ~ "MS", cod_uf == "51" ~ "MT", cod_uf == "52" ~ "GO", cod_uf == "53" ~ "DF")) |>
+  mutate(chaveA = paste(sigla_uf, trimws(str_to_lower(rm_accent(get(nome_mun_col)))), sep = " ")) |>
+  mutate(chaveB = trimws(str_to_lower(rm_accent(get(nome_mun_col)))))
 
-AR_mov_massa <- st_read(dsn = AR_gpkg, layer = "merge_mm_julho22") %>%
-  mutate(chave1 = str_c(UF, trimws(str_to_lower(rm_accent(MUNICIPIO))), sep = " "),
-         chaveA = trimws(str_to_lower(rm_accent(MUNICIPIO))),
-         CLASSE = "GEO")
+UF <- unique(municipios$sigla_uf)
 
-AR_mov_massa <- st_zm(AR_mov_massa, drop = TRUE, what = "ZM")
 
-AR_hidrologico <- st_read(dsn = AR_gpkg, layer = "merge_rh_julho2022_ultimaversao") %>%
-  mutate(chave1 = str_c(UF, trimws(str_to_lower(rm_accent(MUNICIPIO))), sep = " "),
-         chaveA = trimws(str_to_lower(rm_accent(MUNICIPIO))),
-         CLASSE = "HIDRO")
+areas_risco_codmun <- stringdist_left_join(AR_Lote, municipios, by = c("chave1" = "chave2"), max_dist = 0)
 
-AR_hidrologico <- st_zm(AR_hidrologico, drop = TRUE, what = "ZM")
+##############################################
+## tentar implementar como função recursiva ##
+##############################################
 
-areas_risco <- bind_rows(AR_mov_massa, AR_hidrologico)
-
-areas_risco_codmun <- stringdist_left_join(areas_risco, municipios_2021, by = c("chave1" = "chave2"), max_dist = 0)
-
-areas_risco_assoc1 <- areas_risco_codmun %>%
-  filter(!is.na(chave2))
-
-areas_risco_N_assoc1 <- areas_risco_codmun %>%
-  filter(is.na(chave2)) %>%
-  select(-col_municipios)
-
-areas_risco_assoc2 <- list()
-
-for (i in UF) {
-  areas <- filter(areas_risco_N_assoc1, UF == i)
-  municipios <- filter(municipios_2021, SIGLA == i)
-  associado <- stringdist_left_join(areas, municipios, by = c("chaveA" = "chaveB"), max_dist = 1)
-  areas_risco_assoc2[[i]] = associado
-}
-
-areas_risco_assoc2 <- bind_rows(areas_risco_assoc2)
-
-areas_risco_N_assoc2 <- areas_risco_assoc2 %>%
-  filter(is.na(chaveB)) %>%
-  select(-all_of(col_municipios)) %>%
-  mutate(chaveA = case_when(chaveA == "vicencia\r\nvicencia" ~ "vicencia",
-                            chaveA == "visconde do rio brancovisconde do rio branco" ~ "visconde do rio branco",
-                            chaveA == "maranacau" ~ "maracanau"))
-
-areas_risco_assoc2 <- areas_risco_assoc2 %>%
-  filter(!is.na(chaveB))
-
-areas_risco_assoc3 <- list()
-
-for (i in UF) {
-  areas <- filter(areas_risco_N_assoc2, UF == i)
-  municipios <- filter(municipios_2021, SIGLA == i)
-  associado <- stringdist_left_join(areas, municipios, by = c("chaveA" = "chaveB"), max_dist = 1)
-  areas_risco_assoc3[[i]] = associado
-}
-
-areas_risco_assoc3 <- bind_rows(areas_risco_assoc3)
-
-areas_risco_associadas <- bind_rows(areas_risco_assoc1, areas_risco_assoc2, areas_risco_assoc3)
-
-colunas_apagar <- c("SIGLA", "CLASSE", "chave1", "chaveA", "chave2", "chaveB")
-
-areas_risco_hidrologico <- areas_risco_associadas %>%
-  filter(CLASSE == "HIDRO") %>%
-  select(-all_of(colunas_apagar))
-
-areas_risco_mov_massa <- areas_risco_associadas %>%
-  filter(CLASSE == "GEO") %>%
-  select(-all_of(colunas_apagar))
-
-st_write(areas_risco_hidrologico, dsn = "../../result/merge_rh_julho2022_ultimaversao_codIBGE.shp", layer_options = "ENCODING=UTF-8", delete_layer = TRUE)
-
-st_write(areas_risco_mov_massa, dsn = "../../result/merge_mm_julho22_codIBGE.shp", layer_options = "ENCODING=UTF-8", delete_layer = TRUE)
+# areas_risco_assoc1 <- areas_risco_codmun %>%
+#   filter(!is.na(chave2))
+# 
+# areas_risco_N_assoc1 <- areas_risco_codmun %>%
+#   filter(is.na(chave2)) %>%
+#   select(-mun_col)
+# 
+# areas_risco_assoc2 <- list()
+# 
+# for (i in UF) {
+#   areas <- filter(areas_risco_N_assoc1, UF == i)
+#   municipios <- filter(municipios_2021, SIGLA == i)
+#   associado <- stringdist_left_join(areas, municipios, by = c("chaveA" = "chaveB"), max_dist = 1)
+#   areas_risco_assoc2[[i]] = associado
+# }
+# 
+# areas_risco_assoc2 <- bind_rows(areas_risco_assoc2)
+# 
+# areas_risco_N_assoc2 <- areas_risco_assoc2 %>%
+#   filter(is.na(chaveB)) %>%
+#   select(-all_of(col_municipios)) %>%
+#   mutate(chaveA = case_when(chaveA == "vicencia\r\nvicencia" ~ "vicencia",
+#                             chaveA == "visconde do rio brancovisconde do rio branco" ~ "visconde do rio branco",
+#                             chaveA == "maranacau" ~ "maracanau"))
+# 
+# areas_risco_assoc2 <- areas_risco_assoc2 %>%
+#   filter(!is.na(chaveB))
+# 
+# areas_risco_assoc3 <- list()
+# 
+# for (i in UF) {
+#   areas <- filter(areas_risco_N_assoc2, UF == i)
+#   municipios <- filter(municipios_2021, SIGLA == i)
+#   associado <- stringdist_left_join(areas, municipios, by = c("chaveA" = "chaveB"), max_dist = 1)
+#   areas_risco_assoc3[[i]] = associado
+# }
+# 
+# areas_risco_assoc3 <- bind_rows(areas_risco_assoc3)
+# 
+# areas_risco_associadas <- bind_rows(areas_risco_assoc1, areas_risco_assoc2, areas_risco_assoc3)
+# 
+# colunas_apagar <- c("SIGLA", "CLASSE", "chave1", "chaveA", "chave2", "chaveB")
+# 
+# areas_risco_hidrologico <- areas_risco_associadas %>%
+#   filter(CLASSE == "HIDRO") %>%
+#   select(-all_of(colunas_apagar))
+# 
+# areas_risco_mov_massa <- areas_risco_associadas %>%
+#   filter(CLASSE == "GEO") %>%
+#   select(-all_of(colunas_apagar))
+# 
+# st_write(areas_risco_hidrologico, dsn = "../../result/merge_rh_julho2022_ultimaversao_codIBGE.shp", layer_options = "ENCODING=UTF-8", delete_layer = TRUE)
+# 
+# st_write(areas_risco_mov_massa, dsn = "../../result/merge_mm_julho22_codIBGE.shp", layer_options = "ENCODING=UTF-8", delete_layer = TRUE)
--- src/kits.R
+++ src/kits.R
@@ -1,21 +1,69 @@
 library(sf)
 library(rstudioapi)
+library(dplyr)
+
+output <- "./data/"
 
 municipios_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/MUNICIPIOS.gpkg"
 AR_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/AreasDeRisco.gpkg"
 faces_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/Faces.gpkg"
-setores_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/"
+tabfaces_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/Faces_CNEFE.gpkg"
+setores_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/BASES/Setores.gpkg"
 BATER_gpkg <- "W:/DGC_ACERVO_CGEO/PROJETOS_EM_ANDAMENTO/Cemaden/DESENVOLVIMENTO/_GPKG/PRODUTOS/BATER.gpkg"
 
 lotes <- st_layers(AR_gpkg)
-setores_gpkg <- st_layers(setores_gpkg)
-faces <- st_layers(faces_gpkg)
-municipios <- st_layers(municipios_gpkg)
+setores_ver <- st_layers(setores_gpkg)
+faces_ver <- st_layers(faces_gpkg)
+tabfaces_ver <- st_layers(tabfaces_gpkg)
+municipios_ver <- st_layers(municipios_gpkg)
 
-lote_layer <- select.list(lotes[[1]], title = "selecione o lote:", graphics = TRUE)
-face_layer <- select.list(faces[[1]], title = "selecione a base de faces:", graphics = TRUE)
-setor_layer <- select.list(setores_gpkg[[1]], title = "selecione a base de setores:", graphics = TRUE)
-municipios_layer <- select.list(municipios[[1]], title = "selecione a base de municipios:", graphics = TRUE)
+lote_layer <- select.list(lotes[[1]], title = "áreas de risco:", graphics = TRUE)
+faces_layer <- select.list(faces_ver[[1]], title = "base de faces:", graphics = TRUE)
+tabfaces_layer <- select.list(tabfaces_ver[[1]], title = "dados de faces:", graphics = TRUE)
+setor_layer <- select.list(setores_ver[[1]], title = "base de setores:", graphics = TRUE)
+municipios_layer <- select.list(municipios_ver[[1]], title = "base de municipios:", graphics = TRUE)
 
-query_mun <- paste("SELECT DISTINCT ")
-lista_mun <- read_sf()
+col_AR <- read_sf(AR_gpkg, query = paste("SELECT * from ", lote_layer, " LIMIT 0", sep = "")) |> colnames()
+cod_AR <- select.list(col_AR, title = "código dos municípios:", graphics = TRUE)
+
+col_set <- read_sf(setores_gpkg, query = paste("SELECT * from ", setor_layer, " LIMIT 0", sep = "")) |> colnames()
+codmun_set <- select.list(col_set, title = "código dos municípios:", graphics = TRUE)
+
+col_face <- read_sf(faces_gpkg, query = paste("SELECT * from ", faces_layer, " LIMIT 0", sep = "")) |> colnames()
+cod_face <- select.list(col_face, title = "código das faces:", graphics = TRUE)
+
+col_tabface <- read_sf(tabfaces_gpkg,  query = paste("SELECT * from ", tabfaces_layer, " LIMIT 0", sep = "")) |> colnames()
+cod_tabface <- select.list(col_tabface, title = "código das faces", graphics = TRUE)
+
+# tem que resolver a questão dos municípios novos não presentes na base de 2010 - são só dois.
+lista_mun <- read_sf(AR_gpkg,
+                     query = paste("SELECT DISTINCT ", cod_AR, " FROM ", lote_layer)) |> pull() |> as.character()
+
+lista_mun <- lista_mun[1]
+### agora que começam as paradas
+
+aval_quali <- list()
+
+for (i in 1:length(lista_mun)) {
+  areas_risco <- read_sf(AR_gpkg, query = paste("SELECT * FROM ", lote_layer, " WHERE ", cod_AR, " = '", lista_mun[i], "'", sep = ""))
+  setores <- read_sf(setores_gpkg, query = paste("SELECT * FROM ", setor_layer, " WHERE ", codmun_set, " = '", lista_mun[i], "'", sep = ""))
+  faces <- read_sf(faces_gpkg, query = paste("SELECT * FROM ", faces_layer, " WHERE substr(", cod_face, ", 1, 7) = '", lista_mun[i], "'", sep = ""))
+  tabela_faces <- read_sf(tabfaces_gpkg, query = paste("SELECT * FROM ", tabfaces_layer, " WHERE substr(", cod_tabface, ", 1, 7) = '", lista_mun[i], "'", sep = ""))
+  faces <- full_join(faces, tabela_faces, by = setNames(nm = cod_face, cod_tabface), keep = TRUE)
+  aval_quali[[i]] <- data.frame(
+    municipio = lista_mun[i], 
+    faces_com_dado = sum(!is.na(faces[cod_face]) && !is.na(faces[cod_tabface])),#nrow(filter(faces, !is.na(cod_face))), 
+    faces_sem_dado = sum(is.na(faces[cod_tabface])),
+    faces_sem_geo = sum(is.na(faces[cod_face])))
+  faces2 <- faces |> filter(!is.na(cod_face))
+  # gpkg <- paste(output, lista_mun[i], ".gpkg", sep = "")
+  # write_sf(AR, dsn = gpkg, layer = areas_risco)
+  # write_sf(setores, dsn = gpkg, layer = setores)
+  # write_sf(faces, dsn = gpkg, layer = faces)
+}
+
+gpkg <- paste(output, lista_mun[i], "/", lista_mun[i], sep = "")
+write_sf(areas_risco, paste(gpkg, "_areas_risco", ".shp", sep = ""))
+write_sf(setores, paste(gpkg, "_setores", ".shp", sep = ""))
+write_sf(faces, paste(gpkg, "_faces", ".shp", sep = ""))
+
